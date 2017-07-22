411b1fea10585504d9b591c790e1f404aab98663
thumbd12856 <thumbd12856@gmail.com>
2017-07-22 16:39:32 +0800

    nodejs update. xlsx book data update with category.

--- nodejs/insertBookData.js
+++ nodejs/insertBookData.js
@@ -1,4 +1,4 @@
-var filename = './data/test.xlsx';
+var filename = './data/new-test.xlsx';
 var configs = require('./config.js');
 var xlsx = require('node-xlsx');
 var mysql = require('mysql');
@@ -12,35 +12,48 @@ function readFile() {
 
     // parses a file
     var data = xlsx.parse(filename)[0]['data'];
-    var i;
-    for(i = 1; i < data.length; i++) {
-        var insertData = {
-            'bookType': data[i][1] === undefined ? '' : data[i][1],
-            'author': data[i][2] === undefined ? '' : data[i][2],
-            'publicationDate': data[i][3] === undefined ? '' : data[i][3],
-            'title': data[i][4] === undefined ? '' : data[i][4],
-            'bookName': data[i][5] === undefined ? '' : data[i][5],
-            'editor': data[i][6] === undefined ? '' : data[i][6],
-            'publishingLocation': data[i][7] === undefined ? '' : data[i][7],
-            'publisher': data[i][8] === undefined ? '' : data[i][8],
-            'period': data[i][9] === undefined ? '' : data[i][9],
-            'chapter': data[i][10] === undefined ? '' : data[i][10],
-            'page': data[i][11] === undefined ? '' : data[i][11],
-            'department': data[i][12] === undefined ? '' : data[i][12],
-            'thesis': data[i][13] === undefined ? '' : data[i][13],
-            'ISBN': data[i][15] === undefined ? '' : data[i][15],
-            'ISSN': data[i][16] === undefined ? '' : data[i][16]
+    var i, j;
+
+    for(i = 1, j = 1; i < data.length; i++) {
+        if(data[i] == "") {
+            continue;
+        }
+
+        var bookData  = {
+            'bookType':             data[i][1] === undefined ? '' : data[i][1],
+            'author':               data[i][3] === undefined ? '' : data[i][3],
+            'publicationDate':      data[i][4] === undefined ? '' : data[i][4],
+            'title':                data[i][5] === undefined ? '' : data[i][5],
+            'bookName':             data[i][6] === undefined ? '' : data[i][6],
+            'editor':               data[i][7] === undefined ? '' : data[i][7],
+            'publishingLocation':   data[i][8] === undefined ? '' : data[i][8],
+            'publisher':            data[i][9] === undefined ? '' : data[i][9],
+            'period':               data[i][10] === undefined ? '' : data[i][10],
+            'chapter':              data[i][11] === undefined ? '' : data[i][11],
+            'page':                 data[i][12] === undefined ? '' : data[i][12],
+            'department':           data[i][13] === undefined ? '' : data[i][13],
+            'thesis':               data[i][14] === undefined ? '' : data[i][14],
+            'ISBN':                 data[i][16] === undefined ? '' : data[i][16],
+            'ISSN':                 data[i][17] === undefined ? '' : data[i][17]
         };
 
-        DBinsert(insertData);
+        DBinsert('bookData', bookData);
+
+        var classificationData = {
+            'bookId': j++,
+            'typeId': data[i][1],
+            'categoryId': data[i][2]
+        };
+
+        DBinsert('bookClassification', classificationData);
     }
 
     connection.end();
     console.log('insert book data done!');
 }
 
-function DBinsert(data) {
-    connection.query('INSERT INTO `bookData` SET ?', data, function(error) {
+function DBinsert(table, data) {
+    connection.query('INSERT INTO `' + table + '` SET ?', data, function(error) {
         if(error) {
             throw error;
         }
